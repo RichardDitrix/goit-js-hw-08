ff93574bbeac5d2936d9c89b09ca24c91e881d77
Vladimir Tsymbaliuk <89538231+RichardDitrix@users.noreply.github.com>
2023-11-03 21:25:29 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,3 +3,34 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+
+const galleryItemMarkup = ({ preview, original, description }) => {
+	return `
+	
+	  <a class="gallery__item" href="${original}">
+	  <img 
+	  style="display:block"
+	  class="gallery__image"
+	  src="${preview}"
+	  alt="${description}"
+	  />
+	</a>
+	`;
+ };
+
+const makeGalleryMarkup = galleryItems.map(galleryItemMarkup).join('');
+
+const galleryElements = document.querySelector('.gallery');
+
+galleryElements.insertAdjacentHTML('beforeend', makeGalleryMarkup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- js/02-video.js
+++ js/02-video.js
@@ -1,1 +1,18 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const KEY = 'videoplayer-current-time';
+
+const vimeoPlayer = new Player('vimeo-player');
+const key = localStorage.getItem(KEY);
+if (key) {
+	vimeoPlayer.setCurrentTime(parseFloat(key));
+}
+
+vimeoPlayer.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem(KEY, data.seconds.toString());
+  }, 1000),
+);
